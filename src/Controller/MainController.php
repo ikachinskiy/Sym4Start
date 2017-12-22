f77e5bd83aa071ef7f8120ea008c444c6b1b46dd
Kachinskiy Ivan <ivan@kachinskiy.ru>
2017-12-23 01:14:10 +0300

    Компонента Symfony/Form протестирована на простейшей форме в связке с Bootstrap - работает

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,9 +8,13 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\DBAL\Driver\Connection;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
 
 class MainController extends Controller
 {
@@ -18,13 +22,22 @@ class MainController extends Controller
     /**
      * @Route("/", name="index")
      */
-    public function index(Connection $conn) {
+    public function index(Request $request, Connection $conn) {
 
 //        $users = $conn->fetchAll('SELECT * FROM users');
 
-        return $this->render('main/index.html.twig', [
+        $form = $this->createFormBuilder()
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->getForm();
+
+//        return $this->render('main/index.html.twig', [
 //            'users' =>  $users
+//        ]);
+        return $this->render('main/new.html.twig', [
+            'form' => $form->createView(),
         ]);
+
     }
 
 }
